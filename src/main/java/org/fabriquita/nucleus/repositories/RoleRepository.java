f9158d6608342c7f98aa2d4ea1f12a4b9793e2e7
Gabriel Rivera Safadi <gabriel.rivera.safadi@gmail.com>
2015-05-18 01:05:00 -0400

    Add Role visibility.

--- src/main/java/org/fabriquita/nucleus/repositories/RoleRepository.java
+++ src/main/java/org/fabriquita/nucleus/repositories/RoleRepository.java
@@ -1,11 +1,19 @@
 package org.fabriquita.nucleus.repositories;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
+import org.fabriquita.nucleus.models.Group;
 import org.fabriquita.nucleus.models.Role;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 @Transactional
 public interface RoleRepository extends PagingAndSortingRepository<Role, Long> {
     public Role findByName(String name);
+
+    public Page<Role> findByGroupIn(Pageable pageable, List<Group> groups);
+    public Role findByIdAndGroupIn(Long id, List<Group> groups);
 }
--- src/main/java/org/fabriquita/nucleus/services/RoleService.java
+++ src/main/java/org/fabriquita/nucleus/services/RoleService.java
@@ -6,10 +6,13 @@ import org.fabriquita.nucleus.models.Group;
 import org.fabriquita.nucleus.models.Resource;
 import org.fabriquita.nucleus.models.Role;
 import org.fabriquita.nucleus.models.RoleResource;
+import org.fabriquita.nucleus.models.User;
 import org.fabriquita.nucleus.repositories.GroupRepository;
 import org.fabriquita.nucleus.repositories.ResourceRepository;
 import org.fabriquita.nucleus.repositories.RoleRepository;
 import org.fabriquita.nucleus.repositories.RoleResourceRepository;
+import org.fabriquita.nucleus.repositories.UserRepository;
+import org.fabriquita.nucleus.shiro.ShiroSecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -25,19 +28,25 @@ public class RoleService {
 
     @Autowired
     ResourceRepository resourceRepository;
-    
+
     @Autowired
     RoleResourceRepository roleResourceRepository;
-    
+
     @Autowired
     GroupRepository groupRepository;
-    
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    GroupService groupService;
+
     public List<Role> list() {
         return Lists.newLinkedList(roleRepository.findAll());
     }
 
     public Role get(Long id) {
-        return roleRepository.findOne(id);
+        return roleRepository.findByIdAndGroupIn(id, getVisibilityGroups());
     }
 
     public Role add(String name, String description, Long groupId) {
@@ -45,6 +54,9 @@ public class RoleService {
         Group group = null;
         if (groupId != null) {
             group = groupRepository.findOne(groupId);
+            if (!groupService.isVisibleDownAndGroup(getVisibilityGroup(), group)) {
+                return null;
+            }
         }
         role.setName(name);
         role.setDescription(description);
@@ -53,7 +65,7 @@ public class RoleService {
     }
 
     public Role update(Long id, String name, String description, Long groupId) {
-        Role role = roleRepository.findOne(id);
+        Role role = roleRepository.findByIdAndGroupIn(id, getVisibilityGroups());
         Group group = null; 
         if (name != null) {
             role.setName(name);
@@ -63,6 +75,9 @@ public class RoleService {
         }
         if (groupId != null) {
             group = groupRepository.findOne(groupId);
+            if (!groupService.isVisibleDownAndGroup(getVisibilityGroup(), group)) {
+                return null;
+            }
         }
         if(group != null){
             role.setGroup(group);
@@ -71,8 +86,8 @@ public class RoleService {
     }
     
     public Role addRoleResource(Long roleId, Long resourceId, String permissions){
-        Role role = roleRepository.findOne(roleId);
-        Resource resource = resourceRepository.findOne(resourceId);
+        Role role = roleRepository.findByIdAndGroupIn(roleId, getVisibilityGroups());
+        Resource resource = resourceRepository.findOne(resourceId); // TODO grisaf add resource visibility
         RoleResource roleResource = new RoleResource();
         roleResource.setRole(role);
         roleResource.setResource(resource);
@@ -86,13 +101,29 @@ public class RoleService {
     }
 
     public void delete(Long id) {
-        Role role = roleRepository.findOne(id);
+        Role role = roleRepository.findByIdAndGroupIn(id, getVisibilityGroups());
         roleRepository.delete(role);
     }
 
     public Page<Role> list(Integer page, Integer size) {
         PageRequest pageRequest = new PageRequest(page, size);
-        return roleRepository.findAll(pageRequest);
+        return roleRepository.findByGroupIn(pageRequest, getVisibilityGroups());
+    }
+
+    private Group getVisibilityGroup() {
+        User currentUser = userRepository.findOne(ShiroSecurityUtils.getCurrentUserId());
+        if (currentUser == null) {
+            if (ShiroSecurityUtils.isPopdb()) {
+                return groupRepository.findOne(1L);
+            }
+            return null;
+        }
+        return currentUser.getGroup();
+    }
+
+    private List<Group> getVisibilityGroups() {
+        List<Group> groups = groupService.getDownGroupsAndGroup(getVisibilityGroup());
+        return groups;
     }
 
 }
